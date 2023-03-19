1cc0b731c4bb8fd390877d7939abe0598bcdb61a
FloreDenis2001 <floredenis907@yahoo.com>
2023-03-19 18:16:27 +0200

    first_commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [val,setVal]=useState(0);//initializam variabila
+
+  let x=7;
+
+  let test=()=>{
+
+      x++;
+
+      setVal(val+1);      
+
+
+      console.table(x);
+      console.table(val);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Fara {x}</p>
+      <p>State {val}</p>
+
+      <button onClick={test}>Test</button>
     </div>
   );
 }
